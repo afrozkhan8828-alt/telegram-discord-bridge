de2a04676d04011b605ca20098366e659e380651
afrozkhan8828-alt <afrozkhan8828@gmail.com>
2025-12-28 02:29:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-import requests
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
-import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.message.from_user
-    text = update.message.text
+    await update.message.reply_text(update.message.text)
 
-    message = (
-        "📩 **New Telegram Message**\n\n"
-        f"👤 Name: {user.first_name}\n"
-        f"🔗 Username: @{user.username}\n"
-        f"💬 Message:\n{text}"
-    )
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.run_polling()
 
-    requests.post(DISCORD_WEBHOOK, json={"content": message})
-
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-app.run_polling()
+if __name__ == "__main__":
+    main()
